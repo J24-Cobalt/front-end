4d5597d2b313b447583561c7da81bff28b28dac3
SebuBergman <basti.bergman@proton.me>
2024-11-08 16:52:34 +0200

    initialized front-end

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@config": path.resolve(__dirname, "./src/app/config"),
+    },
+  },
+  plugins: [react()],
+});
