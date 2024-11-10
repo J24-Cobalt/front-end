18f8c62171fedea2956ec2ab0373d6c9c86e04c3
SebuBergman <basti.bergman@proton.me>
2024-11-10 04:01:11 +0200

    Would this fix the url

--- src/app/services/api/auth.ts
+++ src/app/services/api/auth.ts
@@ -2,7 +2,7 @@
 import { CompanyData, HasMatchedCompanies, HasMatchesWCompanies, UserData } from "@features/types";
 import axios from "axios";
 
-const API_BASE_URL = "http://136.244.85.199:8001/";
+const API_BASE_URL = "http://136.244.85.199:8001";
 console.log(API_BASE_URL);
 
 // Register user (applicant)
