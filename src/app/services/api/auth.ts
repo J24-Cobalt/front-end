b5a85be4d0b93f3057f9cdaa39ffe4107c2e1582
SebuBergman <basti.bergman@proton.me>
2024-11-10 03:50:57 +0200

    fixed endpoint for api

--- src/app/services/api/auth.ts
+++ src/app/services/api/auth.ts
@@ -2,7 +2,8 @@
 import { CompanyData, HasMatchedCompanies, HasMatchesWCompanies, UserData } from "@features/types";
 import axios from "axios";
 
-const API_BASE_URL = "http://[2001:19f0:6c00:128e:5400:5ff:fe29:3d8f]:8001/";
+const API_BASE_URL = "http://136.244.85.199:8001/";
+console.log(API_BASE_URL);
 
 // Register user (applicant)
 export const registerUser = async (userData: {
